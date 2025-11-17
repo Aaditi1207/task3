04fd1261be2dabe0b1eb7666e11438a46c32149e
aaditi katole <akatole@localhost.localdomain>
2025-11-17 14:18:36 +0530

    Task 3 - Terraform Docker provisioning

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "my-nginx"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
